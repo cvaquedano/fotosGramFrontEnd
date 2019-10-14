082da9e2209ce5d9163c8f8c3b1f8245d958c9d7
Carlos Vaquedano <chvb2002@hotmail.com>
2019-10-14 15:58:58 -0600

    adding user services and login interfaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { AppComponent } from './app.component';
 
 import {HttpClientModule} from '@angular/common/http';
 
+import { IonicStorageModule } from '@ionic/storage';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -18,6 +20,7 @@ import {HttpClientModule} from '@angular/common/http';
     IonicModule.forRoot(),
     AppRoutingModule,
     HttpClientModule,
+    IonicStorageModule.forRoot(),
   ],
   providers: [
     StatusBar,
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { IonSlides } from '@ionic/angular';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { NgForm } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +13,90 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  @ViewChild('slidePrincipal') slides: IonSlides;
+
+  avatars = [
+    {
+      img: 'av-1.png',
+      seleccionado: true
+    },
+    {
+      img: 'av-2.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-3.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-4.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-5.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-6.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-7.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-8.png',
+      seleccionado: false
+    },
+];
+
+avatarSlide = {
+  slidesPerView: 3.5
+};
+
+loginUser = {
+  email: 'chvb2002@gmail.com',
+  password: '123456'
+};
+  constructor(private usuarioService: UsuarioService) { }
 
   ngOnInit() {
+    this.slides.lockSwipes(true);
+  }
+
+  login(fLogin: NgForm) {
+
+    if (!fLogin.valid) { return; }
+
+    this.usuarioService.login(this.loginUser.email, this.loginUser.password);
+
+    console.log(this.loginUser);
+
+
+
+  }
+
+  registro(fRegistro: NgForm) {
+    console.log(fRegistro.valid);
+  }
+
+  seleccionarAvatar(avatar) {
+    this.avatars.forEach(av => av.seleccionado = false);
+    avatar.seleccionado = true;
+  }
+
+  mostrarRegistro() {
+    this.slides.lockSwipes(false);
+    this.slides.slideTo(1);
+    this.slides.lockSwipes(true);
+
+  }
+
+  mostrarLogin() {
+    this.slides.lockSwipes(false);
+    this.slides.slideTo(0);
+    this.slides.lockSwipes(true);
+
   }
 
 }
